00daa147407f8f41403ad8809c71a7dca66f824b
Straivers <straivers98@gmail.com>
2020-12-28 20:17:39 -0500

    Update vulkan sdk version

--- sandbox/shaders/compile.bat
+++ sandbox/shaders/compile.bat
@@ -1,3 +1,2 @@
-C:\VulkanSDK\1.2.154.1\Bin32\glslc shader.vert -o vert.spv
-C:\VulkanSDK\1.2.154.1\Bin32\glslc shader.frag -o frag.spv
-pause
+C:\VulkanSDK\1.2.162.0\Bin32\glslc shader.vert -o vert.spv
+C:\VulkanSDK\1.2.162.0\Bin32\glslc shader.frag -o frag.spv
