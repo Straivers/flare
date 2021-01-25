c2c20db755f17c2857cb1099061ac7f882266437
Straivers <straivers98@gmail.com>
2021-01-25 02:37:58 -0500

    Add documentation to object pools and add missing tests

--- flare/modules/core/source/flare/core/memory/pool.d
+++ flare/modules/core/source/flare/core/memory/pool.d
@@ -1,6 +1,7 @@
 module flare.core.memory.pool;
 
 import flare.core.memory.common;
+import flare.core.memory.allocator;
 
 struct MemoryPool {
 public:
@@ -42,6 +43,8 @@ public:
             b.next_index = (cast(uint) i) + 1;
         }
     }
+
+    @disable this(this);
 
     void[] managed_memory() {
         return _start[0 .. _num_blocks * _block_size];
@@ -122,7 +125,7 @@ unittest {
     {
         // Test allocate-free-allocate
         auto m1 = allocator.allocate(1);
-        auto s1 = m1.ptr;
+        const s1 = m1.ptr;
         allocator.deallocate(m1);
         auto m2 = allocator.allocate(allocator.get_optimal_alloc_size(1));
         assert(m2.ptr == s1);
@@ -153,8 +156,6 @@ unittest {
 }
 
 unittest {
-    import flare.core.memory.allocator: test_allocate_api;
-    
     auto allocator = MemoryPool(new void[](48), 16);
     assert(allocator.alignment == 8);
 
@@ -163,7 +164,7 @@ unittest {
     {
         // Test allocate-free-allocate
         auto m1 = allocator.allocate(1);
-        auto s1 = m1.ptr;
+        const s1 = m1.ptr;
         allocator.deallocate(m1);
         auto m2 = allocator.allocate(allocator.get_optimal_alloc_size(1));
         assert(m2.ptr == s1);
@@ -201,14 +202,16 @@ unittest {
     assert(allocator.alignment == 8);
 
     auto m = allocator.allocate(1);
+    auto s = m;
 
     allocator.deallocate(m);
 
-    try {
-        allocator.deallocate(m);
-    } catch (AssertError e) {
-        assert(0);
-    }
+    bool had_error;
+    try
+        allocator.deallocate(s);
+    catch (AssertError e)
+        had_error = true;
+    assert(had_error);
 }
 
 /**
@@ -216,9 +219,17 @@ An object pool stores a fixed pool of objects, which may be allocated and freed
 quickly.
 */
 struct ObjectPool(T) {
-    import flare.core.memory.allocator: Allocator;
-
 public nothrow:
+    /**
+    Initializes the object pool using a preallocated block of memory. Ownership
+    of the memory remains with the caller, but the caller must guarantee that
+    the memory will not be modified except through the pool while the pool
+    exists.
+
+    Params:
+        memory = The block of memory from which object allocations will be
+                 served. It must be a multiple of `object_size!T`.
+    */
     this(void[] memory) {
         _pool = MemoryPool(memory, object_size!T);
 
@@ -226,40 +237,83 @@ public nothrow:
         assert(_pool.alignment == T.alignof);
     }
 
-    this(Allocator source_allocator, size_t pool_size) {
-        _source = source_allocator;
+    /**
+    Initializes the object pool by allocating a block of memory from the base
+    allocator. This block will be automatically cleaned up when the pool is
+    destroyed.
 
-        auto memory = _source.allocate(pool_size * object_size!T);
+    Params:
+        base_allocator =    The allocator that provides the pool's memory.
+        pool_size =         The number of elements to have in the pool.
+    */
+    this(Allocator base_allocator, size_t pool_size) {
+        _base_allocator = base_allocator;
+
+        auto memory = _base_allocator.allocate(pool_size * object_size!T);
         assert(memory);
 
         _pool = MemoryPool(memory, object_size!T);
     }
 
+    @disable this(this);
+
     ~this() {
-        if (_source)
-            _source.deallocate(_pool.managed_memory);
+        if (_base_allocator)
+            _base_allocator.deallocate(_pool.managed_memory);
     }
 
+    /// The alignment of every allocation from this pool, in bytes.
     size_t alignment() const {
         return _pool.alignment;
     }
 
+    /**
+    Tests if the object was allocated from this pool.
+
+    Params:
+        object = The pointer to test
+    
+    Returns: `yes` if the object belongs, `no` otherwise.
+    */
     Ternary owns(PtrType!T object) const {
         return _pool.owns((cast(void*) object)[0 .. object_size!T]);
     }
 
+    /**
+    Allocates a new object and initializes it in place.
+
+    Params:
+        args = Arguments for the object's constructor. May be empty.
+    
+    Returns: A pointer to the object if allocation was successful, or null if it
+             failed.
+    */
     PtrType!T allocate(Args...)(auto scope ref Args args) {
         auto object_pointer = cast(PtrType!T) _pool.allocate(object_size!T);
         return emplace_obj(object_pointer, args);
     }
 
+    /**
+    Deallocates the object returns it to the pool.
+
+    Note: It is an error to call this function with an object not owned by the
+    pool.
+
+    Params:
+        object = The object of the object to deallocate.
+
+    Returns: `true` if the object was successfully returned to the allocator,
+             `false` otherwise.
+    */
     bool deallocate(ref PtrType!T object) {
+        assert(owns(object));
+
         auto mem = (cast(void*) object)[0 .. object_size!T];
         return _pool.deallocate(mem);
     }
 
 private:
-    Allocator _source;
+    Allocator _base_allocator;
     MemoryPool _pool;
 }
 
@@ -271,11 +325,13 @@ that all handles to a specific allocation become invalid once the memory has
 been deallocated.
 */
 struct WeakObjectPool(T) {
-    import flare.core.memory.allocator: Allocator, make_array, dispose;
-
     align (8) struct Handle {
         uint index;
         uint generation;
+
+        bool opCast(T : bool)() const {
+            return this != Handle();
+        }
 
         H to(H)() {
             static assert(H.sizeof == Handle.sizeof);
@@ -302,11 +358,20 @@ struct WeakObjectPool(T) {
     }
 
 public nothrow:
-    this(Allocator source_allocator, size_t pool_size) {
-        _source = source_allocator;
+    /**
+    Initializes the `WeakObjectPool` with `pool_size` objects. Allocation is
+    assumed to succeed, and will panic if it fails.
+
+    Params:
+        base_allocator =    The allocator to allocate the pool from.
+        pool_size =         The number of objects in the pool.
+    */
+    this(Allocator base_allocator, size_t pool_size) {
+        _base_allocator = base_allocator;
         _num_blocks = pool_size;
 
-        _pool = _source.make_array!_Block(pool_size);
+        _pool = _base_allocator.make_array!_Block(pool_size);
+        assert(_pool);
 
         foreach (i; 0 .. _num_blocks) {
             assert(_num_blocks < uint.max);
@@ -317,12 +382,26 @@ public nothrow:
         _pool[0].generation = 1;
     }
 
+    @disable this(this);
+
     ~this() {
         if (_pool)
-            _source.dispose(_pool);
+            _base_allocator.dispose(_pool);
     }
 
+    /**
+    Retrives the object referred to by `handle`.
+    
+    Note: It is an error to retrieve an object using an invalid handle.
+
+    Params:
+        handle = The handle to the object to retrieve.
+    
+    Returns: A pointer or reference to the object identified by `handle`.
+    */
     PtrType!T get(Handle handle) {
+        assert(owns(handle) == Ternary.yes);
+
         auto block = &_pool[handle.index];
 
         if (block.generation == handle.generation)
@@ -331,10 +410,27 @@ public nothrow:
         return null;
     }
 
+    /**
+    Tests if the handle is currently valid.
+
+    Params:
+        handle = The handle to test
+    
+    Returns: `yes` if the handle is valid, `no` otherwise.
+    */
     Ternary owns(Handle handle) {
-        return Ternary(handle == Handle() || _pool[handle.index].generation == handle.generation);
+        return Ternary(handle.index < _pool.length && _pool[handle.index].generation == handle.generation);
     }
 
+    /**
+    Allocates a new object and initializes it in place.
+
+    Params:
+        args = Arguments for the object's constructor. May be empty.
+    
+    Returns: A handle to the object if allocation was successful, or Handle()
+             if it failed.
+    */
     Handle allocate(Args...)(auto scope ref Args args) {
         if (_freelist_index >= _num_blocks)
             return Handle();
@@ -350,6 +446,17 @@ public nothrow:
         return Handle(index, block.generation);
     }
 
+    /**
+    Deallocates the object identified by `handle` and returns it to the pool.
+
+    Note: It is an error to call this function with an invalid handle.
+
+    Params:
+        handle = The handle of the object to deallocate.
+
+    Returns: `true` if the object was successfully returned to the allocator,
+             `false` otherwise.
+    */
     bool deallocate(Handle handle) {
         assert(owns(handle) == Ternary.yes);
 
@@ -377,8 +484,46 @@ private:
         @disable this(this);
     }
 
-    Allocator _source;
+    Allocator _base_allocator;
     size_t _num_blocks;
     _Block[] _pool;
     uint _freelist_index;
+}
+
+unittest {
+    import flare.core.memory.stack: VirtualStackAllocator;
+    import core.exception: AssertError;
+
+    enum num_objects = 8;
+
+    auto allocator = new AllocatorApi!VirtualStackAllocator(4.kib);
+    auto pool = WeakObjectPool!ulong(allocator, num_objects);
+
+    pool.Handle[num_objects] handles;
+    foreach (ref handle; handles) {
+        handle = pool.allocate();
+        assert(pool.owns(handle) == Ternary.yes);
+        assert(handle);
+    }
+
+    assert(pool.get(handles[0]));
+
+    assert(!pool.allocate());
+
+    pool.deallocate(handles[0]);
+    assert(pool.owns(handles[0]) == Ternary.no);
+
+
+    bool had_error;
+    try
+        pool.get(handles[0]);
+    catch (AssertError e)
+        had_error = true;
+    assert(had_error);
+
+
+    const h1 = pool.allocate();
+    assert(handles[0] != h1);
+
+    assert(!pool.allocate());
 }
