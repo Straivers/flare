9e49ecbb112544b0ecd4af85678d9b59768bec54
Straivers <straivers98@gmail.com>
2021-02-13 00:46:25 -0500

    Implement id allocation

--- sandbox/source/resources.d
+++ sandbox/source/resources.d
@@ -1,32 +1,36 @@
 module resources;
 
-import flare.core.math.util : round_to_next;
 import flare.vulkan;
 
 /**
 Describes the location of an allocation, and the features that it must support.
 */
-enum MemoryType {
+enum MemoryType : ubyte {
+    Unknown = 0,
+
     /// Device-local memory for data that is read often and written rarely. Use
     /// this for meshes, persistent textures, etc.
-    Static,
+    Static = 1,
 
     /// Device-local memory for data that is read often and read a few times.
     /// Use this for uniforms.
-    Dynamic,
+    Dynamic = 2,
 
     /// Host-local memory for data for transferring data to the device.
-    Transfer,
+    Transfer = 3,
 
     /// Host-local memory that is cached for reading dynamic data from the
     /// device.
-    Readback
+    Readback = 4,
+
+    /// The number of supported memory types.
+    Count
 }
 
 /**
 Describes the intended use for a memory allocation.
 */
-enum MemoryUsage {
+enum MemoryUsage : ubyte {
     Unknown         = 0,
     VertexBuffer    = 1 << 0,
     IndexBuffer     = 1 << 1,
@@ -40,21 +44,24 @@ struct BufferAllocInfo {
     uint size;
 }
 
-align (4) struct AllocHandle {
-    void[4] value;
+align (8) struct AllocationId {
+    void[8] value;
 
-    bool opCast(T: bool)() {
+    bool opCast(T: bool)() const {
         return *(cast(ulong*) &value[0]) != 0;
     }
 }
 
 struct DeviceBuffer {
+    import flare.core.math.util : round_to_next;
+
     struct Partition {
         uint size;
         uint alignment;
     }
 
-    AllocHandle handle;
+public:
+    AllocationId handle;
     uint offset;
     uint size;
 
@@ -90,107 +97,169 @@ struct DeviceBuffer {
 }
 
 class VulkanResourceManager {
-    import std.algorithm : filter, map, sum, min;
-    import std.bitmanip : bitfields;
-    import flare.core.memory : kib, vm_alloc, vm_commit, vm_free;
+    import flare.core.memory.virtual: vm_alloc, vm_commit, vm_free;
 
-    /// The smallest allocation size that can fully tile the device's memory.
-    /// This size governs the number of user-facing allocations that are
-    /// supported. For example, a device with 6 gib of VRAM will permit 393216
-    /// allocations total.
-    enum min_average_alloc_size = 16.kib;
-
-    // 2 million allocations. Should be plenty, right?
+    /// 2 Million Allocations
+    // At 8 bytes per index, 2 ^ 20 slots gives 16 mib for the table
     enum max_allocations = 2 ^^ 20;
-
-    enum init_log_message_template = 
-"Initializing Vulkan memory manager.
-\tTotal Device Memory:              %s bytes
-\tMax user allocations:             %s bytes
-\tUnaddressable memory size:        %s bytes
-\tAllocation table size (virtual):  %s bytes";
 
 public:
     this(VulkanDevice device) {
         _device = device;
 
         vkGetPhysicalDeviceMemoryProperties(device.gpu.handle, &_memory_info);
-        
-        const device_memory_size = _memory_info.memoryHeaps[0 .. _memory_info.memoryHeapCount]
-            .filter!(h => (h.flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT) != 0)
-            .map!(h => h.size)
-            .sum();
 
-        assert(min_average_alloc_size < device_memory_size, "Device memory less than tiling size?? This GPU is not supported");
+        auto mem = vm_alloc(max_allocations * AllocationId_.sizeof)[0 .. max_allocations];
+        vm_commit(mem);
 
-        const num_tiles = min(device_memory_size / min_average_alloc_size, max_allocations);
-        const unusable_space = device_memory_size % min_average_alloc_size;
+        _id_pool = cast(AllocationId_[]) mem;
 
-        const table_size = num_tiles * BufferAllocSlot.sizeof;
-        _buffers = cast(BufferAllocSlot[]) vm_alloc(table_size)[0 .. table_size];
-        assert(_buffers.length == num_tiles);
-
-        vm_commit(_buffers);
-
-        device.context.logger.info(init_log_message_template, device_memory_size, num_tiles, unusable_space, table_size);
+        // Initialize first ID so that we can use AllocationId(0) as 'null'.
+        _first_free_slot_index = 0;
+        _num_touched_ids = 1;
+        _id_pool[0].index_or_next = AllocationId_.max_u20;
+        _id_pool[0].generation = 1;
     }
 
     ~this() {
-        vm_free(_buffers);
+        vm_free(_id_pool);
     }
 
     DeviceBuffer allocate(ref BufferAllocInfo info) {
-        auto slot = allocate_slot();
+        auto id = _allocate_id();
+        
+        _memory_pools[info.type].allocate(info, id);
 
-        // do allocation
-
-        return DeviceBuffer();
+        return DeviceBuffer(id.handle, 0, 0);
     }
 
     void deallocate(DeviceBuffer buffer) {
-        auto index = Index(buffer.handle);
+        const id = AllocationId_(buffer.handle);
+        assert(_verify(id));
 
-        deallocate_slot(index);
+        _memory_pools[id.pool_type].deallocate(buffer);
+
+        _deallocate_id(id);
     }
 
 private:
-    union BufferAllocSlot {
-        Index index;
-        struct {
-            uint size;
-            VkBuffer buffer;
+    bool _verify(AllocationId_ id) {
+        // Chort-circuit so we won't access untouched (and unbacked) memory.
+        if (id.index_or_next < _num_touched_ids)
+            return false;
+
+        const id_mask = id.mask_value & (~AllocationId_.manager_data_mask);
+        const pool_id_mask = _id_pool[id.index_or_next].mask_value & (~AllocationId_.manager_data_mask);
+
+        return id_mask == pool_id_mask;
+    }
+
+    AllocationId_ _allocate_id() {
+        if (_first_free_slot_index != AllocationId_.max_u20) {
+            const index = _first_free_slot_index;
+            auto pool_id = &_id_pool[index];
+
+            pool_id.index_or_next = index;
+            pool_id.is_live = true;
+
+            _first_free_slot_index = pool_id.index_or_next;
+
+            return *pool_id;
         }
+
+        if (_num_touched_ids == _id_pool.length)
+            assert(0, "Ran out of allocation IDs");
+
+        const index = _num_touched_ids;
+        _num_touched_ids++;
+
+        auto pool_id = &_id_pool[index];
+        pool_id.index_or_next = index;
+        pool_id.is_live = true;
+
+        return *pool_id;
     }
 
-    union Index {
-        AllocHandle handle;
+    void _deallocate_id(AllocationId_ id) {
+        assert(_verify(id));
+        
+        auto pool_id = &_id_pool[id.index_or_next];
+        assert(pool_id.is_live);
 
-        struct {
-            mixin(bitfields!(
-                uint,"offset", 20,
-                uint,"generation", 12
-            ));
+        // Clear all bits except index and generation (is_live = false)
+        pool_id.mask_value = pool_id.mask_value & AllocationId_.index_generation_mask;
+
+        const index = pool_id.index_or_next;
+        pool_id.index_or_next = _first_free_slot_index;
+
+        // If the generation is not saturated, add it back to the pool
+        if (pool_id.generation < AllocationId_.max_u20) {
+            pool_id.generation = pool_id.generation + 1;
+            _first_free_slot_index = index;
         }
-    }
-
-    BufferAllocSlot* allocate_slot() {
-        return null;
-    }
-
-    void deallocate_slot(Index index) {
-
     }
 
     VulkanDevice _device;
     VkPhysicalDeviceMemoryProperties _memory_info;
 
-    BufferAllocSlot* _buffer_freelist;
-    BufferAllocSlot[] _buffers;
-    size_t _active_buffer_slots;
+    uint _first_free_slot_index;
+    uint _num_touched_ids;
+    AllocationId_[] _id_pool;
+
+    VulkanMemoryPool[MemoryType.Count] _memory_pools;
 }
 
-class VulkanStagingManager {
-    this(VulkanResourceManager resources) {
+private:
+
+union AllocationId_ {
+    import std.bitmanip : bitfields;
+
+    enum max_u20 = 2 ^^ 20 - 1;
+
+    /// Mask with all bits for index_or_next and generation members set.
+    static immutable ulong index_generation_mask;
+
+    /// Mask with all bits for 
+    static immutable ulong manager_data_mask;
+
+    shared static this() {
+        AllocationId_ id_generation;
+        id_generation.index_or_next = AllocationId_.max_u20;
+        id_generation.generation = AllocationId_.max_u20;
+        AllocationId_.index_generation_mask = id_generation.mask_value;
+        
+        AllocationId_ manager_data;
+        manager_data.impl_u20 = AllocationId_.max_u20;
+        AllocationId_.manager_data_mask = manager_data.mask_value;
+    }
+
+public:
+    AllocationId handle;
+    ulong mask_value;
+
+    struct {
+        mixin(bitfields!(
+            // ResourceManager data
+            uint,       "index_or_next",    20,
+            uint,       "generation",       20,
+            bool,       "is_live",          1,
+            MemoryType, "pool_type",        3,
+
+            // MemoryPool data
+            uint,       "impl_u20",         20,
+        ));
+    }
+}
+
+struct VulkanMemoryPool {
+
+    @disable this(this);
+
+    DeviceBuffer allocate(ref BufferAllocInfo info, ref AllocationId_ id) {
+        assert(0);
+    }
+
+    void deallocate(DeviceBuffer allocation) {
 
     }
 }
